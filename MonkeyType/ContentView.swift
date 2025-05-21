43c9717afe29ea4c41a2981bde8c5483bbc95edd
yjkh17 <yj.kh17@gmail.com>
2025-05-21 18:31:20 +0300

    Initial Commit

---
+++ MonkeyType/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MonkeyType
+//
+//  Created by Yousef Jawdat on 21/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MonkeyType/MonkeyTypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonkeyTypeApp.swift
+//  MonkeyType
+//
+//  Created by Yousef Jawdat on 21/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct MonkeyTypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
